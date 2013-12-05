c35a88f6ea4e4714dfb1e056713a72c1b282875b
Chris de Villiers <chrisdevilliers@gmail.com>
2013-12-05 18:29:10 +0200

    Fixed postgresql locale and encoding.

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,5 +5,8 @@ apt_repository 'pgdg' do
     key          'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
 end
 
+# Setup locale before installing postgresql otherwise we end up with latin1 encoded databases
+execute "update-locale LANG=en_US.utf8 LC_ALL=en_US.utf8"
+
 package "postgresql-9.3"
 package "postgresql-contrib-9.3"
